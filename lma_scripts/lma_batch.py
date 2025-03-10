7d16009fc2752903a76834e2b08493ede2c24e75
D.J. Figueroa <djfigs10@gmail.com>
2025-03-10 22:49:38 +0000

    better termination with interrupts

--- lma_scripts/lma_batch.py
+++ lma_scripts/lma_batch.py
@@ -5,8 +5,11 @@ from typing import Optional
 from lma_data.LMA_data_file import LMADataFile
 from lma_data.LMA_browser import LMABrowser
 from rich.progress import Progress
+from threading import Event, Lock
 
 lma_processes: list[subprocess.Popen[str]] = []
+lma_process_lock = Lock()
+lma_shutdown_event = Event()
 
 
 def process_batch(batch: list[LMADataFile], lma_analysis_args: str, silent_mode: bool):
@@ -19,29 +22,43 @@ def process_batch(batch: list[LMADataFile], lma_analysis_args: str, silent_mode:
     duration = 300
     data_files = " ".join([data_file.path for data_file in batch])
     cmd = f"lma_analysis -d {date} -t {time} -s {duration} {lma_analysis_args} {data_files}"
-    lma_analysis_process = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-    )
-    lma_processes.append(lma_analysis_process)
+    
+    # Attempt to create the process
+    with lma_process_lock:
+        if lma_shutdown_event.is_set():
+            return
+        
+        lma_analysis_process = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        lma_processes.append(lma_analysis_process)
 
     if not silent_mode:
         for line in lma_analysis_process.stdout:
             print(f"[{lma_datetime}] {line}", end="")
 
     lma_analysis_process.wait()
-    lma_processes.remove(lma_analysis_process)
+
+    with lma_process_lock:
+        lma_processes.remove(lma_analysis_process)
 
 
-def setup_signal_catcher(executor: ThreadPoolExecutor):
+def setup_signal_catcher():
     def on_exit(signum, frame):
-        executor.shutdown(wait=False, cancel_futures=True)
-        print("Stopping analysis...")
-        for lma_process in lma_processes:
-            lma_process.terminate()
+        did_already_shutdown = lma_shutdown_event.is_set()
+        if not did_already_shutdown:
+            print("Terminating lma_analysis processes...")
 
-        exit()
+        with lma_process_lock:
+            lma_shutdown_event.set()
 
-    signal.signal(signal.SIGINT, on_exit)
+            for lma_process in lma_processes:
+                if did_already_shutdown:
+                    lma_process.kill()
+                else:
+                    lma_process.terminate()
+
+    signal.signal(signal.SIGTERM, on_exit)
 
 
 def process_batches(
@@ -55,7 +72,7 @@ def process_batches(
 
     with Progress() as lma_progress:
         with ThreadPoolExecutor(max_workers=num_workers) as executor:
-            setup_signal_catcher(executor)
+            setup_signal_catcher()
 
             futures = [
                 executor.submit(
