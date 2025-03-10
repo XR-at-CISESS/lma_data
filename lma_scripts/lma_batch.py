da197cb56b2a19686ec85c4b26af70019b423dc3
D.J. Figueroa <djfigs10@gmail.com>
2025-03-10 17:11:41 +0000

    add num workers argument

--- lma_scripts/lma_batch.py
+++ lma_scripts/lma_batch.py
@@ -1,6 +1,7 @@
-import argparse, subprocess
+import argparse, subprocess, os
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+from typing import Optional
 from lma_data.LMA_data_file import LMADataFile
 from lma_data.LMA_browser import LMABrowser
 
@@ -8,7 +9,7 @@ from lma_data.LMA_browser import LMABrowser
 def process_batch(batch: list[LMADataFile], lma_analysis_args: str):
     if len(batch) == 0:
         return
-    
+
     lma_datetime = batch[0].datetime
     date = lma_datetime.strftime("%Y%m%d")
     time = lma_datetime.strftime("%H%M%S")
@@ -18,8 +19,16 @@ def process_batch(batch: list[LMADataFile], lma_analysis_args: str):
     subprocess.run(cmd, shell=True)
     print(f"Done {lma_datetime}")
 
-def process_batches(batches, lma_analysis_args: str):
-    with ThreadPoolExecutor(max_workers=16) as executor:
+
+def process_batches(
+    batches: list[list[LMADataFile]],
+    lma_analysis_args: str,
+    num_workers: Optional[int] = None,
+):
+    if not num_workers:
+        num_workers = os.cpu_count()
+
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         for batch in batches:
             executor.submit(process_batch, batch, lma_analysis_args)
 
@@ -32,9 +41,8 @@ def create_parser():
     parser.add_argument("data_dir")
     parser.add_argument("start_date")
     parser.add_argument("end_date")
-    parser.add_argument(
-        dest="lma_analysis_args", nargs=argparse.REMAINDER, default=""
-    )
+    parser.add_argument(dest="lma_analysis_args", nargs=argparse.REMAINDER, default="")
+    parser.add_argument("--num_workers", type=int, dest="num_workers")
     return parser
 
 
@@ -54,10 +62,11 @@ def main():
 
     start = parse_date_string(args.start_date)
     end = parse_date_string(args.end_date)
+    num_workers = args.num_workers
 
     batches = browser.batch(start, end)
     lma_analysis_args = " ".join(args.lma_analysis_args)
-    process_batches(batches, lma_analysis_args)
+    process_batches(batches, lma_analysis_args, num_workers)
 
 
 if __name__ == "__main__":
